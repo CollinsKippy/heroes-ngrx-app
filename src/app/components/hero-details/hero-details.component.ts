b65b4afa27e6c18a9bf8997cdbb53b7b35a1e435
CK <collins@analence.com>
2023-02-14 19:59:53 +0300

    hero components

---
+++ src/app/components/hero-details/hero-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-hero-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './hero-details.component.html',
+  styleUrls: ['./hero-details.component.scss']
+})
+export class HeroDetailsComponent {
+
+}
---
+++ src/app/components/hero-list/hero-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-hero-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './hero-list.component.html',
+  styleUrls: ['./hero-list.component.scss']
+})
+export class HeroListComponent {
+
+}
