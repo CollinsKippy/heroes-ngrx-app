bf46ab1982235b8b246522d4d5a5effc1ad10ddd
CK <collins@analence.com>
2023-02-14 21:22:33 +0300

    navbar routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationLink } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  navigationLinks: NavigationLink[] = [
+    { name: 'Dashboard', route: '/' },
+    { name: 'Heroes', route: '/heroes' },
+  ];
+  appName: string = 'Perforce';
   title = 'heroes-ngrx-app';
 }
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
+export type NavigationLink = {
+  name: string;
+  route: string;
+};
+
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, RouterModule],
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
-
+  @Input() navigationLinks: NavigationLink[] | undefined = [];
+  @Input() appName: string | undefined;
 }
