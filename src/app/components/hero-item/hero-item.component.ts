52d95d9f39ca06adb6594091cdfedc1086e320ba
CK <collins@analence.com>
2023-02-15 10:23:41 +0300

    ui updates

--- src/app/components/hero-item/hero-item.component.ts
+++ src/app/components/hero-item/hero-item.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { Hero } from 'src/app/entities/hero';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-hero-item',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatDividerModule],
+  imports: [CommonModule, MatCardModule, MatDividerModule, MatButtonModule],
   templateUrl: './hero-item.component.html',
   styleUrls: ['./hero-item.component.scss']
 })
